355c394243e1855b96447f9be2d3cd933842469b
Yuz <516418828@qq.com>
2019-09-29 14:45:29 +0800

    添加数据库创建语句和insert语句

--- src/main/java/com/entity/Card.java
+++ src/main/java/com/entity/Card.java
@@ -15,6 +15,24 @@ import lombok.experimental.Accessors;
 @AllArgsConstructor
 @Accessors(chain = true)
 public class Card {
+
+
+//    CREATE TABLE `card` (
+//            `id` int(11) NOT NULL AUTO_INCREMENT,
+//  `user_id` varchar(255) DEFAULT NULL,
+//  `card_no` int(11) DEFAULT NULL,
+//  `card_name` varchar(255) DEFAULT NULL,
+//    PRIMARY KEY (`id`)
+//) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+//
+//    INSERT INTO `demo`.`card`(`id`, `user_id`, `card_no`, `card_name`) VALUES (1, '0', 0, 'string');
+//    INSERT INTO `demo`.`card`(`id`, `user_id`, `card_no`, `card_name`) VALUES (2, '1', 22, 'name2');
+//    INSERT INTO `demo`.`card`(`id`, `user_id`, `card_no`, `card_name`) VALUES (3, '2', 33, 'name3');
+//    INSERT INTO `demo`.`card`(`id`, `user_id`, `card_no`, `card_name`) VALUES (4, '2', 44, 'name4');
+//    INSERT INTO `demo`.`card`(`id`, `user_id`, `card_no`, `card_name`) VALUES (6, '1', 1, 'string');
+
+
+
     private int id;
     private int userId;
     private int cardNo;
--- src/main/java/com/entity/User.java
+++ src/main/java/com/entity/User.java
@@ -16,6 +16,20 @@ import lombok.experimental.Accessors;
 @Accessors(chain = true)
 public class User {
 
+//    CREATE TABLE `user` (
+//            `id` int(11) NOT NULL AUTO_INCREMENT,
+//  `name` varchar(255) DEFAULT NULL,
+//  `address` varchar(255) DEFAULT NULL,
+//    PRIMARY KEY (`id`)
+//) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+
+//    INSERT INTO `demo`.`user`(`id`, `name`, `address`) VALUES (1, '张三', 'add:1111');
+//    INSERT INTO `demo`.`user`(`id`, `name`, `address`) VALUES (2, '李四', 'add:2222');
+//    INSERT INTO `demo`.`user`(`id`, `name`, `address`) VALUES (3, '王五', 'add:3333');
+//    INSERT INTO `demo`.`user`(`id`, `name`, `address`) VALUES (4, '赵六', 'add:4444');
+//    INSERT INTO `demo`.`user`(`id`, `name`, `address`) VALUES (5, '小明', 'add:5555');
+
+
     private Integer id;
     private String name;
     private String address;
